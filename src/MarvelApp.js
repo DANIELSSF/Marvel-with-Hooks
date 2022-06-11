7be1213c1983e3e1074bdd48e65a33b4c128bfc9
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-10 22:46:55 -0500

    This is the main component

---
+++ src/MarvelApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Marvel } from './Components/Marvel'
+
+export const MarvelApp = () => {
+  return (
+    <div className='container'>
+      <h1>MarvelApp</h1>
+      <hr />
+      <Marvel />
+    </div>
+  )
+}
