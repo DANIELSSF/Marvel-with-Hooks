f31fb224c2b42f40bbce171dd93bfba4d346a7ef
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-11 21:55:52 -0500

    This hook getter the data from the api

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -3,19 +3,27 @@ import { useEffect, useState } from 'react';
 export const useData = () => {
     const url = 'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=ee0235138aac484e9e8bc18a99276f9f&hash=07857ef4ad731a30ff23e9ff9f8ec93a';
 
+
     const [state, setState] = useState([]);
 
     useEffect(() => {
         fetch(url)
             .then(resp => resp.json())
-            .then(data => {
-                data.map(datas => {
-                    setState({
-                        id: datas.data.results.id,
-                        title: datas.data.results.title,
-                        description: datas.data.results.description,
-                        images: datas.data.results.images.path`/portrait_incredible.jpg`,
-                    });
+            .then(datas => {
+                const { data } = datas;
+                const { results } = data;
+                results.map(info => {
+                    setState(st => (
+                        [...st, {
+                            id: info.id,
+                            title: info.title,
+                            description: info.description,
+                            images: info.thumbnail.path,
+                            size: '/portrait_incredible.',
+                            extension: info.thumbnail.extension,
+
+                        }]
+                    ));
                 });
             });
     }, [url])
