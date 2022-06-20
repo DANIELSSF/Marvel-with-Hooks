2f042458a912cbc768a90a1627a18cbf675b194c
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-20 17:30:49 -0500

    The component now has a conditional to redirect to a MarvelSearch.js or to a MarvelComics.js with default data

--- src/Components/Marvel.js
+++ src/Components/Marvel.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MarvelComics } from './Comics/MarvelComics';
 
 import '../index.css'
 import { MarvelNav } from './Comics/MarvelNav';
+import { useFetchSearch } from '../Hooks/SearchIndexComcis/useFecthSearch';
+
+import { MarvelSearch } from './Search/MarvelSearch';
 
 export const Marvel = () => {
+
+    const { loadingSearch } = useFetchSearch();
+
+    // const loading = useEffect(() => {
+    //     return ();
+    // }, [loadingSearch]);
 
 
     return (
         <>
             <MarvelNav />
             <div className='comicsItem'>
-                <MarvelComics />
+                {!loadingSearch ? <MarvelSearch /> : <MarvelComics />}
             </div>
 
         </>
