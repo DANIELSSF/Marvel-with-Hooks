45018f160a13bf7d3ff28f0392061389e6d2f745
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-20 17:32:23 -0500

    The Hook now has a limit of 50 data elements

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export const useData = () => {
-    const url = 'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=ee0235138aac484e9e8bc18a99276f9f&hash=07857ef4ad731a30ff23e9ff9f8ec93a';
+    const url = 'http://gateway.marvel.com/v1/public/comics?limit=50&ts=1&apikey=ee0235138aac484e9e8bc18a99276f9f&hash=07857ef4ad731a30ff23e9ff9f8ec93a';
 
 
     const [state, setState] = useState([]);
