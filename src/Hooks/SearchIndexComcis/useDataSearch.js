f061cd70a7260d865a3a812d55d4abaf5deb81b0
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-20 17:37:22 -0500

    This hook has extracted the API data with a search comic

---
+++ src/Hooks/SearchIndexComcis/useDataSearch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+export const useDataSearch = (name) => {
+    const url = `http://gateway.marvel.com/v1/public/comics?title=${name}&limit=20&ts=1&apikey=ee0235138aac484e9e8bc18a99276f9f&hash=07857ef4ad731a30ff23e9ff9f8ec93a`;
+
+    const [state, setState] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then(resp => resp.json())
+            .then(datas => {
+                const { data } = datas;
+                const { results } = data;
+                results.map(info => {
+                    setState(st => (
+                        [...st, {
+                            id: info.id,
+                            title: info.title,
+                            description: info.description,
+                            images: `${info.thumbnail.path}/portrait_incredible.jpg`,
+
+                        }]
+                    ));
+                });
+            });
+    }, [url])
+
+    return state;
+
+}
