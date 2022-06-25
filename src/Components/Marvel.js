5c5cfb8de9e6e3d54b4bebd204e909933830f7b6
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-24 22:38:56 -0500

    A useState has been added to the component for information storage

--- src/Components/Marvel.js
+++ src/Components/Marvel.js
@@ -1,27 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { MarvelComics } from './Comics/MarvelComics';
-
-import '../index.css'
 import { MarvelNav } from './Comics/MarvelNav';
-import { useFetchSearch } from '../Hooks/SearchIndexComcis/useFecthSearch';
+import '../index.css'
 
-import { MarvelSearch } from './Search/MarvelSearch';
+export const Marvel = React.memo(() => {
 
-export const Marvel = () => {
-
-    const { loadingSearch } = useFetchSearch();
-
-    useEffect(() => {
-    }, [loadingSearch]);
-
+    const [stateMarvel, setStateMarvel] = useState('');
 
     return (
         <>
-            <MarvelNav />
+            <MarvelNav setStateMarvel={setStateMarvel} />
             <div className='comicsItem'>
-                {!loadingSearch ? <MarvelSearch /> : <MarvelComics />}
+                <MarvelComics stateMarvel={stateMarvel} />
             </div>
 
         </>
     )
-}
+})
