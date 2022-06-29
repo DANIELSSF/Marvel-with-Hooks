0fb737a9ce1d99cfd5f2a70eaf89ba941a82848d
Daniel Silva <daniel.santiago730@gmail.com>
2022-06-29 01:50:38 -0500

    The component principal now have a useContext

--- src/MarvelApp.js
+++ src/MarvelApp.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import { Marvel } from './Components/Marvel'
+import React, { useState } from 'react'
+import { BrowserRouter, useLocation } from 'react-router-dom'
+import { AppRoutes } from './Components/AppRoutes'
+import { marvelContext } from './Components/helpers/useContext'
 
 export const MarvelApp = () => {
+
+  const [stateMarvel, setStateMarvel] = useState('');
+
+  const [navSearch, setNavSearch] = useState(true);
+
+  
   return (
     <div >
-      <Marvel />
+      <BrowserRouter>
+        <marvelContext.Provider value={{
+          stateMarvel,
+          setStateMarvel,
+          navSearch,
+          setNavSearch
+        }}>
+
+          <AppRoutes />
+        </marvelContext.Provider>
+      </BrowserRouter>
     </div>
   )
 }
