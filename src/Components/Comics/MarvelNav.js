26a648cdc76557ffec5201c5f120aa8b089043b1
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-01 16:17:53 -0500

    The navbar was uptdate

--- src/Components/Comics/MarvelNav.js
+++ src/Components/Comics/MarvelNav.js
@@ -1,38 +1,15 @@
-import React, { useContext } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useForm } from '../../Hooks/useForm';
-import { marvelContext } from '../helpers/useContext';
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { MarvelNavSearch } from './MarvelNavSearch';
 
 export const MarvelNav = () => {
-
-    const { setStateMarvel } = useContext(marvelContext);
-    const location = useLocation();
-    const { pathname } = location;
-
-    const [state, handleInputComic] = useForm({
-        ip: ''
-    });
-
-    const { ip } = state;
-    const name = ip.replace(" ", "%20");
-
-    const handleInput = (e) => {
-        e.preventDefault();
-
-
-        if (name.length < 2) {
-            return;
-        };
-
-        setStateMarvel(name);
-    }
 
     return (
         <nav className="navbar navbar-dark ">
             <div className="container-fluid">
-                <Link className="navbar-brand mx-auto" to="/" >
+                <NavLink className="navbar-brand mx-auto" to="/" >
                     <img src="https://tm.ibxk.com.br/2018/06/08/08172409055350.jpg?ims=1120x420" alt="" width="290" height="90" ></img>
-                </Link>
+                </NavLink>
                 <button className="navbar-toggler navbar-to" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -45,27 +22,14 @@ export const MarvelNav = () => {
                     <div className="offcanvas-body">
                         <ul className="navbar-nav justify-content-end flex-grow-1 pe-4" >
                             <li className="nav-item">
-                                <a className="nav-link active" to='/'>Comics</a>
+                                <Link className="nav-link active" to='/'>Comics</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" >Favoritos</a>
+                                <Link className="nav-link" to='/favorites'>Favoritos</Link>
                             </li>
                         </ul>
                         <br />
-                        {pathname === '/' &&
-                            <form className="d-flex " role="search" onSubmit={handleInput}>
-                                <input className="form-control me-2"
-                                    type="search"
-                                    placeholder="Search Comic..."
-                                    aria-label="Search"
-                                    autoComplete='off'
-                                    name='ip'
-                                    onChange={handleInputComic}
-                                    value={ip}
-                                ></input>
-                                <button className="btn btn-outline-danger" type="submit">Searchs</button>
-                            </form>
-                        }
+                        <MarvelNavSearch />
                     </div>
                 </div>
             </div>
---
+++ src/Components/Comics/MarvelNavSearch.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react'
+import { useLocation } from 'react-router-dom';
+import { useForm } from '../../Hooks/Forms/useForm';
+import { marvelContext } from '../helpers/useContext';
+
+export const MarvelNavSearch = () => {
+
+    const { setStateMarvel } = useContext(marvelContext);
+    const location = useLocation();
+    const { pathname } = location;
+
+    const [state, handleInputComic] = useForm({
+        ip: ''
+    });
+
+    const { ip } = state;
+    const name = ip.replace(" ", "%20");
+
+    const handleInput = (e) => {
+        e.preventDefault();
+
+
+        if (name.length < 2) {
+            return;
+        };
+
+        setStateMarvel(name);
+    }
+
+
+    return (
+        <>
+            {pathname === '/' &&
+                <form className="d-flex " role="search" onSubmit={handleInput}>
+                    <input className="form-control me-2"
+                        type="search"
+                        placeholder="Search Comic..."
+                        aria-label="Search"
+                        autoComplete='off'
+                        name='ip'
+                        onChange={handleInputComic}
+                        value={ip}
+                    ></input>
+                    <button className="btn btn-outline-danger" type="submit">Searchs</button>
+                </form>
+            }
+        </>
+    )
+}
