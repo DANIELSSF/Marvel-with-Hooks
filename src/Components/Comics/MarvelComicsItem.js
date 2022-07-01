757a58030fdefd4866bae30f3defc7e300e83fb4
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-01 16:19:21 -0500

    Animations was added

--- src/Components/Comics/MarvelComicsItem.js
+++ src/Components/Comics/MarvelComicsItem.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import 'animate.css';
 
 export const MarvelComicsItem = ({ id, title, images }) => {
 
     return (
         <>
-            <div className='col'>
+            <div className='col animate__animated animate__fadeIn '>
                 <Link to={`/comic/${id}`}>
                     <div className="card 
                             border-danger
-                            text-bg-dark 
-                            cardComics "
+                            text-bg-dark
+                            cardComics"
                     >
-                        <img src={images} className="card-img-top" alt={title}></img>
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
+                        <div>
+                            <figure>
+                                <img src={images} className="card-img-top " alt={title}></img>
+                            </figure>
+                                <div className="card-body ">
+                                    <h5 className="card-title ">{title}</h5>
+                                </div>
                         </div>
                     </div>
                 </Link>
@@ -25,6 +30,7 @@ export const MarvelComicsItem = ({ id, title, images }) => {
 }
 MarvelComicsItem.propTypes = {
     title: PropTypes.string.isRequired,
-    images: PropTypes.string.isRequired
+    images: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired
 }
 
