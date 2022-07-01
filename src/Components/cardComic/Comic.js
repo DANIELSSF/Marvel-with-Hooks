77e6a6a7b500f040d27cdf2a7099412354202356
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-01 16:21:28 -0500

    The component now send the id to useComicId.js

--- src/Components/cardComic/Comic.js
+++ src/Components/cardComic/Comic.js
@@ -1,13 +1,26 @@
-import React, { useContext } from 'react'
-import {  useParams } from 'react-router-dom';
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useComicID } from '../../Hooks/ComicID/useComicID';
+import { ComicID } from './ComicID';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const Comic = () => {
 
-
-  const {id} = useParams();
-
+  const { id } = useParams();
+  const { data, loading } = useComicID(id);
+  console.log(data, loading);
 
   return (
-    <div>Comic</div>
+    <>
+
+      {!loading ? <div className='spinner'> <Spinner animation="border" variant="danger" /></div>
+        :
+        <div className='container'>
+          {
+            <ComicID data={data} />
+          }
+        </div>
+      }
+    </>
   )
 }
