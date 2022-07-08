e96a04b23e983d34b9d870d973aa1e7352d6bec5
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-07 21:29:57 -0500

    The todoReducer helper has actions such as add and removed that use the useReducer hook

--- src/helpers/todoReducer.js
+++ src/helpers/todoReducer.js
@@ -4,6 +4,8 @@ export const todoReducer = (state, action) => {
     switch (action.type) {
         case 'add':
             return [...state, action.payload];
+        case 'removed':
+            return state.filter(todo => todo.id !== action.payload);
 
         default:
             return state;
