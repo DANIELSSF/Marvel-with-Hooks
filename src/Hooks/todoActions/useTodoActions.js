b73ced15fe5cd19d07ad17220e635b5e6753d428
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-07 21:38:19 -0500

    The hook uses fuctions as handleTodoAdd and handleTodoRemoved with the hook useReducer and uses the localStorage

---
+++ src/Hooks/todoActions/useTodoActions.js
@@ -0,0 +1,36 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../../helpers/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todoComic')) || [];
+}
+
+export const useTodoActions = () => {
+
+    const [todoComic, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todoComic', JSON.stringify(todoComic));
+    }, [todoComic]);
+
+
+    const handleTodoAdd = (newComicTodo) => {
+        const action = {
+            type: 'add',
+            payload: newComicTodo
+        };
+
+        dispatch(action);
+        console.log(action);
+    };
+
+    const handleTodoRemoved = (todoId) => {
+        dispatch({
+            type: 'removed',
+            payload: todoId
+        });
+
+    };
+    console.log(todoComic);
+    return [todoComic, handleTodoAdd, handleTodoRemoved];
+}
