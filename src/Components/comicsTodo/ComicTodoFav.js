ff0d02b82ad722f1fdfd4862faf56202d11af8d5
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-09 22:02:01 -0500

    React-Boostrap now is used here

--- src/Components/comicsTodo/ComicTodoFav.js
+++ src/Components/comicsTodo/ComicTodoFav.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import { ComicTodoItem } from './ComicTodoItem'
+import { Row } from 'react-bootstrap';
 
-export const ComicTodoFav = ({ todoComic, handleTodoRemoved}) => {
+export const ComicTodoFav = ({ todoComic, handleTodoRemoved }) => {
     return (
-        <div>
-              <div className='col-md-5
-                            col-5'>
-            {
-                todoComic.map(todoComics => (
-                    <ComicTodoItem
-                        key={todoComics.id}
-                        todoComics= {todoComics}
-                        handleTodoRemoved= {handleTodoRemoved}
+        <>
+            <Row xs={0} md={5} className="g-4">
+                {Array.from({ length: 1 }).map((_, idx) => (
+
+                    todoComic.map(todoComics => (
+                        <ComicTodoItem
+                            key={todoComics.id}
+                            todoComics={todoComics}
+                            handleTodoRemoved={handleTodoRemoved}
                         />
-                        ))
-            }
-            </div>
-        </div>
+                    ))
+                ))}
+            </Row>
+        </>
     )
 }
-ComicTodoFav.propTypes={
+ComicTodoFav.propTypes = {
     todoComic: PropTypes.array.isRequired,
     handleTodoRemoved: PropTypes.func.isRequired
 }
