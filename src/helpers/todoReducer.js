8407b33ebbe9ec1ab745b3e918666b06bc09d1d3
Daniel Silva <daniel.santiago730@gmail.com>
2022-07-12 19:24:28 -0500

    todoReducer.js only accepts comics with a unique id

--- src/helpers/todoReducer.js
+++ src/helpers/todoReducer.js
@@ -1,9 +1,25 @@
 
+
 export const todoReducer = (state, action) => {
 
     switch (action.type) {
         case 'add':
-            return [...state, action.payload];
+            const stateComics = [...state, action.payload];
+            const uniqueId = new Set();
+
+            const unique = stateComics.filter(elem => {
+                const isDuplicate = uniqueId.has(elem.id);
+                uniqueId.add(elem.id);
+
+                if (!isDuplicate) {
+                    return true;
+                }
+                return false;
+            });
+
+            console.log(unique);
+            return unique;
+
         case 'removed':
             return state.filter(todo => todo.id !== action.payload);
 
